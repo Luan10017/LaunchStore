f2cf82774e05e8591588399e0f4e0aea7f25996b
Luan Andrade <luan10017@hotmail.com>
2020-10-09 21:41:48 -0300

    Adicionei edição de produtos

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -23,10 +23,18 @@ module.exports = {
 
         let results = await Procuct.create(req.body)
         const productId = results.rows[0].id
+        
+        return res.redirect(`/products/${productId}`)
+    },
+    async edit(req, res) {
+        let results = await Procuct.find(req.params.id)
+        const product = results.rows[0]
+
+        if(!product) return res.send("Product not found!")
 
         results = await Category.all()
         const categories = results.rows
 
-        return res.render("products/create.njk", {productId, categories})
+        return res.render("products/edit.njk", {product, categories})
     }
 }
