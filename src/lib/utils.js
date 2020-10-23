38afe8e93b67ae6d5e8bf3ccd0002a75b16fa75d
Luan Andrade <luan10017@hotmail.com>
2020-10-23 17:18:41 -0300

    Aditei formatação de datas

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,19 +1,19 @@
 module.exports = {
     date(timestamp) {
         const date = new Date(timestamp)
-        
-        //yyyy
-        const year = date.getUTCFullYear()
-        // mm
-        const month = `0${date.getUTCMonth() + 1}`.slice(-2)
-
-        //dd    
-        const day = `0${date.getUTCDate()}`.slice(-2)
+  
+        const year = date.getFullYear()
+        const month = `0${date.getMonth() + 1}`.slice(-2)   
+        const day = `0${date.getDate()}`.slice(-2)
+        const hour = date.getHours()
+        const minutes = date.getMinutes()
         
         return {
             day,
             month,
             year,
+            hour,
+            minutes,
             iso: `${year}-${month}-${day}`,
             birthDay: `${day}/${month}`,
             format: `${day}/${month}/${year}`
